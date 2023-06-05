701090de9a6c8626720e5f57c73790462f4a8611
chldmsql <126857666+chldmsql@users.noreply.github.com>
2023-06-05 14:36:08 +0900

    20230605  14:00
    글쓰기 완료

--- project_mj/src/kr/or/dw/cs/service/CsServiceImpl.java
+++ project_mj/src/kr/or/dw/cs/service/CsServiceImpl.java
@@ -2,8 +2,9 @@ package kr.or.dw.cs.service;
 
 import kr.or.dw.cs.dao.CsDaoImpl;
 import kr.or.dw.cs.dao.ICsDao;
+import kr.or.dw.cs.vo.AnnouncementVO;
 
-public class CsServiceImpl {
+public class CsServiceImpl implements ICsService{
 	private ICsDao dao;
 	private static CsServiceImpl service;
 	
@@ -14,5 +15,17 @@ public class CsServiceImpl {
 	public static CsServiceImpl getInstance() {
 		if (service == null) service = new CsServiceImpl();
 		return service;
+	}
+
+	@Override
+	public int updateContent(AnnouncementVO announcementVO) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int insertContent(AnnouncementVO announcementVO) {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 }
